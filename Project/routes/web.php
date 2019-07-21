fb50fd34109965797dbee538c0322c47c7e401b4
Dung288 <52401650+Dung288@users.noreply.github.com>
2019-07-21 11:56:13 +0700

    Update web.php

--- Project/routes/web.php
+++ Project/routes/web.php
@@ -15,31 +15,36 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('thongtin/{tuoi}/{ten}', function($tuoi, $ten) {
-   return "Hello $ten , $tuoi tuổi";
-});
+// Route::get('thongtin/{tuoi}/{ten}', function($tuoi, $ten) {
+//    return "Hello $ten , $tuoi tuổi";
+// });
 
-Route::get('test', function () {
-	echo 'Hello Would';
-})->name('Route2');
+// Route::get('test', function () {
+// 	echo 'Hello Would';
+// })->name('Route2');
 
-Route::get('hoten/{ten}', function ($ten) {
-	echo 'Ten cua ban la: ' . $ten;
-})->name('Route1');
+// Route::get('hoten/{ten}', function ($ten) {
+// 	echo 'Ten cua ban la: ' . $ten;
+// })->name('Route1');
 
-Route::get('Goiten', function(){
-	return redirect()->route('Route2');
-});
-//group
+// Route::get('Goiten', function(){
+// 	return redirect()->route('Route2');
+// });
+// //group
 
-route::group(['prefix'=>'group'],function(){
-	route::get('user1',function(){
-		echo 'user1';
-	});
-	route::get('user2',function(){
-		echo 'user2';
-	});
-	route::get('user3',function(){
-		echo 'user3';
-	});
-});
+// route::group(['prefix'=>'group'],function(){
+// 	route::get('user1',function(){
+// 		echo 'user1';
+// 	});
+// 	route::get('user2',function(){
+// 		echo 'user2';
+// 	});
+// 	route::get('user3',function(){
+// 		echo 'user3';
+// 	});
+// });
+
+Route::get('index',[
+	'as' => 'trang chu',
+	'uses' => 'PageController@getIndex'
+]);
