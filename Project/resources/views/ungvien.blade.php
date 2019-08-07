14f05d66e70b2f9320e13484bf0712c66efa2102
Nguyễn Mạnh Dũng <42793473+dungnguyen288@users.noreply.github.com>
2019-08-07 19:48:35 +0700

    Update ungvien.blade.php

--- Project/resources/views/ungvien.blade.php
+++ Project/resources/views/ungvien.blade.php
@@ -229,7 +229,7 @@
 <nav class="navbar navbar-inverse" style="margin-bottom: 0px;">
 	<div class="container-fluid">
 		<div class="navbar-header">
-			<a href="trangchu.blade.php"><img src="img/logo.png" class="navbar-brand"></a>
+			<a href="trangchu"><img src="img/logo.png" class="navbar-brand"></a>
 		</div>
 		<ul class="nav navbar-nav">
 			<li><a href="trangchu.blade.php">Trang chủ</a></li>
